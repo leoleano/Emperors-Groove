8751d951733b35a558237e1d3c5aa3d6a18676dd
Miguel Mendoza <mendoza.miguel131@gmail.com>
2025-07-15 13:33:40 -0600

    added code from basic_structure poise example

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,48 @@
 use std::env;
 
+mod commands;
+use poise::{framework, serenity_prelude as serenity, EditTracker};
+use std::{
+    collections::HashMap,
+    env::var,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
+// Types used by all command functions
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+pub struct Data {
+    votes: Mutex<HashMap<String, u32>>,
+}
+
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::prelude::*;
 
 struct Handler;
 
+async fn on_error(error: poise::FrameworkError<'_, Data, Error>){
+    // This is our custom error handler
+    // They are many errors that can occur, so we only handle the ones we want to customize
+    // and forward the rest to the default handler
+    match error {
+        poise::FrameworkError::Setup {error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command {error, ctx, .. } => {
+            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
+
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
+    async fn message(&self, ctx: serenity::Context, msg: Message) {
         println!(
             "recieved: \"{}\" from {}({})",
             msg.content,
@@ -30,6 +64,93 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
+    // // Login with a bot token from the environment
+    // let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    // // Set gateway intents, which decides what events the bot will be notified about
+    // let intents = GatewayIntents::GUILD_MESSAGES
+    //     | GatewayIntents::DIRECT_MESSAGES
+    //     | GatewayIntents::MESSAGE_CONTENT;
+
+    // // Create a new instance of the Client, logging in as a bot.
+    // let mut client = Client::builder(&token, intents)
+    //     .event_handler(Handler)
+    //     .await
+    //     .expect("Err creating client");
+
+    // println!("starting up");
+    // // Start listening for events by starting a single shard
+    // if let Err(why) = client.start().await {
+    //     println!("Client error: {why:?}");
+    // }
+
+    tracing_subscriber::fmt::init();
+
+    // FrameworkOptions contains all of poise's configuration option in one struct
+    // Every option can be omitted to use its default value
+    let options = poise::FrameworkOptions {
+        commands: vec![commands::help(), commands::vote(), commands::getvotes()],
+        prefix_options: poise::PrefixFrameworkOptions {
+            prefix: Some("~".into()),
+            edit_tracker: Some(Arc::new(poise::EditTracker::for_timespan(
+                Duration::from_secs(3600),
+            ))),
+            additional_prefixes: vec![
+                poise::Prefix::Literal("hey bot,"),
+                poise::Prefix::Literal("hey bot"),
+            ],
+            ..Default::default()
+        },
+        // The global error handler for all error cases that may occur
+        on_error: |error| Box::pin(on_error(error)),
+        // This code is run before every command
+        pre_command: |ctx| {
+            Box::pin(async move {
+                println!("Executing command {}...", ctx.command().qualified_name);
+            })
+        },
+        // This code is run after a command if it was successful (returned Ok)
+        post_command: |ctx| {
+            Box::pin(async move {
+                println!("Executed command {}!", ctx.command().qualified_name);
+            })
+        },
+        // Every command invocation must pass this check to continue execution
+        command_check: Some(|ctx| {
+            Box::pin(async move {
+                if ctx.author().id == 123456789 {
+                    return Ok(false);
+                }
+                Ok(true)
+            })
+        }),
+        // Enforce command checks even for owners (enforced by default)
+        // Set to true to bypass checks, which is useful for testing
+        skip_checks_for_owners: false,
+        event_handler: |_ctx, event, _framework, _data| {
+            Box::pin(async move {
+                println!(
+                    "Got an even in event handler: {:?}",
+                    event.snake_case_name()
+                );
+                Ok(())
+            })
+        },
+        ..Default::default()
+    };
+
+    let framework = poise::Framework::builder()
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                println!("Logged in as {}", _ready.user.name);
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {
+                    votes: Mutex::new(HashMap::new()),
+                })
+            })
+        })
+        .options(options)
+        .build();
+
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     // Set gateway intents, which decides what events the bot will be notified about
@@ -40,6 +161,7 @@ async fn main() {
     // Create a new instance of the Client, logging in as a bot.
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
+        .framework(framework)
         .await
         .expect("Err creating client");
 
